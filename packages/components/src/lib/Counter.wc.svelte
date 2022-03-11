87c1b64ba665bb98ddca7627439429ba3ee54e5a
jiayike <19923295+jiayike@users.noreply.github.com>
2022-03-11 20:23:28 +0000

    add eslint and prettier

--- packages/components/src/lib/Counter.wc.svelte
+++ packages/components/src/lib/Counter.wc.svelte
@@ -1,13 +1,13 @@
-<script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
-</script>
-
 <svelte:options tag="my-counter" />
 
-<button on:click={increment}>
+<script lang="ts">
+  let count = 0;
+  const increment = (): void => {
+    count += 1;
+  };
+</script>
+
+<button on:click="{increment}">
   Clicks: {count}
 </button>
 
