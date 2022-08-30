eecabfe97f2f5c91a23ea2e26e2c4d2d645e03a3
jiayike <kahyee4567@hotmail.com>
2022-08-30 17:15:36 +0000

    build: migrate storybook vite to new package

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,11 +1,8 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/html",
   core: {
-    builder: "storybook-builder-vite",
-  },
+    builder: "@storybook/builder-vite"
+  }
 };
