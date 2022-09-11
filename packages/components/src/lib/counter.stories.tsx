f4108af265ca18b02d5c0da2aaaa62f6b9e40446
jiayike <19923295+jiayike@users.noreply.github.com>
2022-09-11 18:34:41 +0000

    build: vite build --watch in components folder to allow web components stories

---
+++ packages/components/src/lib/counter.stories.tsx
@@ -0,0 +1,11 @@
+import '../../dist/components';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Path/To/MyComponent',
+}
+
+export const Basic = (): string => "<my-counter></my-counter>";
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import * as fs from 'fs';
-
-const COMPONENTS_FOLDER = './src/lib/';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
-      input: fs.readdirSync(COMPONENTS_FOLDER).map((file) => `${COMPONENTS_FOLDER}${file}`),
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: '@pixel-ui/components',
     },
   },
   plugins: [
     svelte({
-      include: '**/*.svelte',
+      include: '**/*.wc.svelte',
     }),
   ],
 });
