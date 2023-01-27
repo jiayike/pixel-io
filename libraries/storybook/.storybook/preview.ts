15eba762f3451b9f051fbcdec68d392fddfa6809
jiayike <19923295+jiayike@users.noreply.github.com>
2023-01-27 17:57:57 +0000

    docs: define ordering in storybook

--- libraries/storybook/.storybook/preview.ts
+++ libraries/storybook/.storybook/preview.ts
@@ -5,6 +5,18 @@ const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const theme = isDarkMode ? themes.dark : themes.light;
 
 export const parameters = {
+  options: {
+    storySort: {
+      order: [
+        "HTML|CSS",
+        ["Quick Start", "Changelog", "Atoms", "Molecules", "Organisms"],
+        "Components",
+        ["Quick Start", "Changelog", "Atoms", "Molecules", "Organisms"],
+        "*",
+      ],
+      locales: "en-US",
+    },
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
