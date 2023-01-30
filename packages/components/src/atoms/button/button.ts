6d5da884c550c6ed9bf47b52a7d836f89c3db941
jiayike <19923295+jiayike@users.noreply.github.com>
2023-01-30 17:12:06 +0000

    feat: button animations

--- packages/components/src/atoms/button/button.ts
+++ packages/components/src/atoms/button/button.ts
@@ -2,8 +2,11 @@ import { LitElement, unsafeCSS, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import buttonStyle from "@pixel-ui/styles/src/atoms/_button.scss?inline";
 import reset from "@pixel-ui/styles/src/base/_reset.scss?inline";
+import animations from "@pixel-ui/styles/src/base/_animations.scss?inline";
 
 type ButtonTypes = "primary" | "secondary";
+type AnimationTypes = "slide-in";
+type AnimationDelayTypes = "1" | "2" | "3" | "4";
 
 /**
  * Button Component.
@@ -12,13 +15,29 @@ type ButtonTypes = "primary" | "secondary";
  */
 @customElement("pixel-button")
 export class PixelButton extends LitElement {
-  static styles = [unsafeCSS(reset), unsafeCSS(buttonStyle)];
+  static styles = [
+    unsafeCSS(reset),
+    unsafeCSS(animations),
+    unsafeCSS(buttonStyle),
+  ];
 
   /**
    * Button style type.
    */
   @property()
   type: ButtonTypes = "primary";
+
+  /**
+   * Button animation type.
+   */
+  @property()
+  animationType?: AnimationTypes;
+
+  /**
+   * Button animation delay.
+   */
+  @property()
+  animationDelay?: AnimationDelayTypes;
 
   private onClick = () => {
     const event = new CustomEvent("onClick", {
@@ -28,10 +47,24 @@ export class PixelButton extends LitElement {
     this.dispatchEvent(event);
   };
 
+  private getAnimationClasses = (): string => {
+    const classes = [];
+    if (this.animationType) {
+      classes.push(`animate__${this.animationType}`);
+    }
+
+    if (this.animationDelay) {
+      classes.push(`animate__delay-${this.animationDelay}`);
+    }
+
+    return classes.join(" ");
+  };
+
   render() {
     return html`
       <button
-        class="pixel-button pixel-button__${this.type}"
+        class="pixel-button pixel-button__${this
+          .type} ${this.getAnimationClasses()}"
         @click=${this.onClick}
       >
         <slot />
