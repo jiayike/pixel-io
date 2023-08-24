ccab79b3d5eb2c86714b948c8b36a298dcfaa601
jiayike <19923295+jiayike@users.noreply.github.com>
2023-08-24 13:03:55 +0000

    build(deps): upgrade dependencies

--- packages/components/vitest-setup.ts
+++ packages/components/vitest-setup.ts
@@ -1,11 +1,1 @@
-import matchers, { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
-
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace jest {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
-    interface Matchers<R = void> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
-  }
-}
-
-expect.extend(matchers);
+import '@testing-library/jest-dom/vitest';
