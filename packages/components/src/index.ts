056b6cf829497b35ef86275c0976df461794992a
jiayike <19923295+jiayike@users.noreply.github.com>
2024-01-25 14:12:17 +0000

    feat: add exports for PixelAnimation and PixelTooltip

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,5 @@
 import '@pixel-io/styles/src/global.scss';
+export { PixelAnimation } from './atoms/animation/animation';
 export { PixelButton } from './atoms/button/button';
+export { PixelTooltip } from './atoms/tooltip/tooltip';
 export { PixelNavigation } from './molecules/navigation/navigation';
